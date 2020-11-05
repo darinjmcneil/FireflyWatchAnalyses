18d2c13b517ab3b6b978a09348ec2dfcba4c5ae5
McNeil Jr <djm6984@psu.edu>
2020-11-05 16:07:02 -0500

    Oklahoma pest map!

--- SpeedyBeeModel_SingleStatePractice.R
+++ SpeedyBeeModel_SingleStatePractice.R
@@ -1,48 +1,35 @@
-install.packages("devtools")
-library(devtools)
-# install.packages("RTools") # Not availbale for R version 4.0.2...?
-# need to download from here: https://cran.r-project.org/bin/windows/Rtools/
-# maybe those aren't needed?
-
-install_github("land-4-bees/SpeedyBeeModel")
+# install.packages("devtools")
+# install_github("land-4-bees/SpeedyBeeModel")
 library(SpeedyBeeModel)
-
-# working directory
-setwd("D:\\Desktop Files November 2019\\fireflies\\insecticide_code")
-
 library(raster)
-# Memory problems -- give R a bit more memory # not sure if this is necessary
-# rasterOptions(memfrac=0.8, tmptime=4)
+library(devtools)
 
 # read in the reclass table
-ReclassTablePath1 <-("E:\\FireflyAnalysis_October2020\\insecticide_reclass_table\\beetox_I_cdl_reclass_20200717.csv")
-ReclassTable1 <- read.csv(ReclassTablePath1)
-head(ReclassTable1)
+ReclassTableDir1 <-("E:\\FireflyAnalysis_October2020\\insecticide_reclass_table") # location of reclass table
+ReclassTablePath1 <- paste0(ReclassTableDir1, "\\beetox_I_cdl_reclass_20200717.csv") # name of reclass table
+ReclassTable1 <- read.csv(ReclassTablePath1) # read in reclass table
 
-# filter recalss table for focal year/state
-# Filter reclass table AND export
-# grab path to filtered table
-# NOTE - Mel is going to try and tweak insecticide_index so it accepts an R object
-# instead of a pathway :-)
-
+# subset reclass table for Oklahoma
 OK2008_reclasstable <- subset(ReclassTable1, state_alpha == "OK" & year == "2008") # subset table for focal state/year
-head(OK2008_reclasstable)
-ReclassTableDir1 <-("E:\\FireflyAnalysis_October2020\\insecticide_reclass_table\\beetox_I_cdl_reclass_20200717")
-write.csv(OK2008_reclasstable, paste0(ReclassTableDir1, "\\OK2008.csv")) # Mel is a GD genius!
+
+# export the subsetted reclass table
+write.csv(OK2008_reclasstable, paste0(ReclassTableDir1, "\\OK2008.csv"))
 
 # define path to CDL raster file
-OK_cdl2008path <- "E:\\FireflyAnalysis_October2020\\CDL_individual\2008\\CDL_2008_Oklahoma.tif"
+OK_cdl2008path <- "E:\\FireflyAnalysis_October2020\\CDL_individual\\2008\\CDL_2008_Oklahoma.tif"
+plot(raster(OK_cdl2008path))
 
 #####################################################
 # convert the CDL to pest map by the subsetted table (via SpeedyBeeModel)
 #####################################################
 
-# Yes set foraging range
-
-# also see ?insecticide_index !!
+# install.packages("logger")
+install.packages("tidyr")
+library(logger)
+library(tidyr)
 
 SpeedyBeeModel::insecticide_index(
-  output_dir = "E:\\FireflyAnalysis_October2020\\individual_pesticide_maps\\2008", # output loc
+  output_dir = "E:\\FireflyAnalysis_October2020\\individual_pesticide_maps\\2008\\OK2008", # output loc
   pesticide_path = paste0(ReclassTableDir1, "\\OK2008.csv"), # path to pesticide FILTERED reclass table
   landcover_path = OK_cdl2008path, # path to each landcover CDL map
   forage_range = 500, # range in meters
@@ -53,8 +40,7 @@ SpeedyBeeModel::insecticide_index(
   check_pesttable = F # leave useW, and rastertag as default
 )
 
-# This fails on large states (ME) but works for smaller states (RI). Error says:
-# Error: cannot allocate vector of size 3.0 Gb
+# Did it work??
 
-
-
+ok2008map <- raster("E:\\FireflyAnalysis_October2020\\individual_pesticide_maps\\2008\\OK2008\\CDL_2008_Oklahoma_insecticide.tif")
+plot(ok2008map)
